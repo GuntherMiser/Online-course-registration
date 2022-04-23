1b246bf61a0359cc02acb802ee81f09c9a909d32
Mehedi hasan sazzad <104253352+GuntherMiser@users.noreply.github.com>
2022-04-23 08:23:33 -0700

    Add files via upload

---
+++ onlinecourse/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 Online Course Registration | By : <a href="http://www.phpgurukul.com/" target="_blank">Mehedi and tasmin</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
